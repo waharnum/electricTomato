19e5504d56622444a49ca86749de19853d40356e
Alan Harnum <waharnum@gmail.com>
2017-05-10 16:48:21 -0400

    Pause/Resume text toggle.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 800})
+  win = new BrowserWindow({width: 400, height: 400})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
